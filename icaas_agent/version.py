cc388d131e6331ec3e4536e9baf0cc31141a8d6e
Nikos Skalkotos <skalkoto@grnet.gr>
2015-04-03 18:23:22 +0300

    Update version.py to v0.1

--- icaas_agent/version.py
+++ icaas_agent/version.py
@@ -1,8 +1,8 @@
 
-__version__ = "0.0next"
+__version__ = "0.1"
 __version_vcs_info__ = {
-    'branch': 'develop',
-    'revid': '9c426b6',
-    'revno': 1}
+    'branch': 'master',
+    'revid': 'd9c6210',
+    'revno': 30}
 __version_user_email__ = "skalkoto@grnet.gr"
 __version_user_name__ = "Nikos Skalkotos"
