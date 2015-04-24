99bc326869015d94a3fbf72b6cb77bd26e2a601d
Nikos Skalkotos <skalkoto@grnet.gr>
2015-04-24 18:45:28 +0300

    Update ChangeLog and version.py for v0.1.2

--- icaas_agent/version.py
+++ icaas_agent/version.py
@@ -1,8 +1,8 @@
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __version_vcs_info__ = {
-    'branch': 'hotfix-0.1.1',
-    'revid': '388a615',
-    'revno': 35}
+    'branch': 'hotfix-0.1.2',
+    'revid': 'bdd1eeb',
+    'revno': 41}
 __version_user_email__ = "skalkoto@grnet.gr"
 __version_user_name__ = "Nikos Skalkotos"
